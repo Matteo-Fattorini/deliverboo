1aff527603ce86dacd55d7d134ffb77616788b25
Giulia <gnapoli1996@gmail.com>
2021-02-26 15:24:02 +0100

    Add auth

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+// LATO CLIENTE
+// Route home che contiene l'elenco dei ristoranti
+
+// route ristorante id oppure nome ristorante 
+
+// nel checkout: al click del bottone 'paga' -> route per pushare l'ordine nel db, serve una resource che prende i dati del cliente, e invia l'ordine al ristoratore 
