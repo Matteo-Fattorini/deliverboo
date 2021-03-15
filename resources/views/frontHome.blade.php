aa9b9ae4f188d278fa76c73e277ec7baa11feee5
Matteo Fattorini <fattorinimatteo@gmail.com>
2021-03-15 11:44:22 +0100

    Minor bug-fixed

--- resources/views/frontHome.blade.php
+++ resources/views/frontHome.blade.php
@@ -3,12 +3,13 @@
     <JoinComponent></JoinComponent>
     <PartnerSectionHome style="background-image: url({{ asset('img/homepage/rider.jpg') }})"
         loginlink="{{ route('login') }}" registerlink="{{ route('register') }}" 
+        auth="{{ isset(Auth::user()->getRestaurant) }}"
         @auth
         @if(isset(Auth::user()->getRestaurant))
         dashboardlink="{{ route('restaurant.show', Auth::user()->getRestaurant->id) }}" 
         @endif
-        @endauth
-        auth="{{ Auth::check() }}">
+        @endauth>
+        
 
 
     </PartnerSectionHome>
